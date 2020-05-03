0d51cb3c9ffe0f9f0d775220b8859457bb1050f5
Monica Rodriguez <monik_0491@hotmail.com>
2020-05-03 12:40:55 -0500

    mongoDB connected to heroku

--- Develop/server.js
+++ Develop/server.js
@@ -17,7 +17,7 @@ app.use(express.static("public"));
 var bodyParser = require('body-parser');
 
 //db Mongo
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://monik_0491:Fermina04.@ds149742.mlab.com:49742/heroku_0zdllnt8";
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false
